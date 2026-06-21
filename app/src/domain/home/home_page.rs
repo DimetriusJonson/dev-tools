6b303ef94c6e40d6e5f07b3aabadfa50c1085562
Dmitriy V. Isaev <shtirlizlive@yandex.ru>
2026-06-21 11:24:00 +0300

    copy to clipboard

--- app/src/domain/home/home_page.rs
+++ app/src/domain/home/home_page.rs
@@ -13,7 +13,7 @@ pub fn HomePage() -> impl IntoView {
     let (ident, set_ident) = signal("4".to_owned());
     let (in_progress, set_in_progress) = signal(false);
 
-    let on_click = move |_| {
+    let on_format_click = move |_| {
         let xml = xml.get();
         spawn_local(async move {
             set_in_progress.set(true);
@@ -32,15 +32,22 @@ pub fn HomePage() -> impl IntoView {
         });
     };
 
+    let on_copy_click = move |_| {
+        if let Some(window) = web_sys::window() {
+            let navigator = window.navigator();
+            let clipboard = navigator.clipboard();
+            let _ = clipboard.write_text(&dst_xml.get());
+        }
+    };
+
     view! {
-        <div class="flex-1 flex flex-row items-stretch gap-4 px-2 py-4 text-xs md:text-base">
+        <div class="flex-1 flex flex-row gap-4 px-2 py-4 text-xs md:text-base">
             <TextArea
                 name="xml".to_owned()
                 class_name="resize-none".to_owned()
                 placeholder="Вставьте xml".to_owned()
                 value=xml
                 set_value=set_xml
-                rows="5".to_owned()
                 on_change=|_| {}
             />
 
@@ -58,20 +65,30 @@ pub fn HomePage() -> impl IntoView {
                     label=">>".to_owned()
                     button_width=ButtonWidth::Md
                     loading=in_progress
-                    on_click=on_click
+                    on_click=on_format_click
                     disabled=in_progress
                 />
             </div>
 
-            <TextArea
-                name="dst_xml".to_owned()
-                class_name="resize-none".to_owned()
-                placeholder="Здесь будет отформатированный xml".to_owned()
-                value=dst_xml
-                set_value=set_dst_xml
-                rows="5".to_owned()
-                on_change=|_| {}
-            />
+            <div class="flex flex-col gap-4 w-full">
+                <TextArea
+                    name="dst_xml".to_owned()
+                    class_name="flex-1 resize-none".to_owned()
+                    placeholder="Здесь будет отформатированный xml".to_owned()
+                    value=dst_xml
+                    set_value=set_dst_xml
+                    on_change=|_| {}
+                />
+
+                <Button class_name="".to_owned()
+                    label="Скопировать в буфер обмена".to_owned()
+                    button_width=ButtonWidth::Auto
+                    loading=in_progress
+                    on_click=on_copy_click
+                    disabled=in_progress
+                />
+
+            </div>
         </div>
     }
 }
