cdcb99c30c7157fc42274b4547fdcebe795bb417
DimetriusJonson <shtirlizlive@gmail.com>
2026-07-12 18:03:11 +0300

    tauri - prevent multiply instances

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -50,6 +50,13 @@ pub fn run() {
                 ])
                 .build(),
         )
+        .plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.unminimize();
+                let _ = window.show();
+                let _ = window.set_focus();
+            }
+        }))
         .setup(|app| {
             let args: Vec<String> = std::env::args().collect();
 
