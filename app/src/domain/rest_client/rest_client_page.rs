6c6d7ed0cc8edb491714faeeb1828c42593404c6
DimetriusJonson <shtirlizlive@gmail.com>
2026-07-22 15:44:32 +0300

    rest_client - save/restore custom headers

--- app/src/domain/rest_client/rest_client_page.rs
+++ app/src/domain/rest_client/rest_client_page.rs
@@ -1,4 +1,5 @@
 use gloo_net::http::Request;
+use leptos::leptos_dom::logging::console_log;
 use leptos::prelude::*;
 use leptos::task::spawn_local;
 
@@ -31,7 +32,7 @@ impl InProgressType {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug)]
 struct CustomHeader {
     id: usize,
     name: ReadSignal<String>,
@@ -55,13 +56,17 @@ pub fn RestClientPage() -> impl IntoView {
 
     let (custom_header_name, set_custom_header_name) = signal("".to_owned());
     let (custom_header_value, set_custom_header_value) = signal("".to_owned());
-    let (custom_headers, set_custom_headers) = signal(Vec::<CustomHeader>::new());
+    let (custom_headers, set_custom_headers) = signal(Vec::new());
 
     let (url, set_url) = signal(get_local_store_value("rc_url", "".to_owned()));
     let (method, set_method) = signal(get_local_store_value("rc_method", "GET".to_owned()));
     let (body, set_body) = signal(get_local_store_value("rc_body", "".to_owned()));
     let (response, set_response) = signal(None);
     let (in_progress, set_in_progress) = signal(InProgressType::None);
+
+    let _ = Effect::new(move || {
+        set_custom_headers.set(restore_custom_headers());
+    });
 
     let on_send_click = move |_| {
         spawn_local(async move {
@@ -232,13 +237,14 @@ pub fn RestClientPage() -> impl IntoView {
                                     !is_base_header_name(&name_converted) &&
                                     !custom_header_value.read_untracked().is_empty() {
                                     if custom_headers.read_untracked().iter().find(|h|h.name.read_untracked().trim().to_lowercase() == name_converted).is_none() {
-                                        let id = custom_headers.read_untracked().len() + 1;
+                                        let id = custom_headers.read_untracked().iter().map(|h|h.id).max().unwrap_or_default() + 1;
                                         let (name, set_name) = signal(custom_header_name.get_untracked());
                                         let (value, set_value) = signal(custom_header_value.get_untracked());
 
                                         set_custom_headers.write().push(CustomHeader{ id, name, set_name, value, set_value });
                                         set_custom_header_name.set("".to_owned());
                                         set_custom_header_value.set("".to_owned());
+                                        save_custom_headers(&custom_headers.read_untracked());
                                     }
                                 }
                             }
@@ -256,7 +262,9 @@ pub fn RestClientPage() -> impl IntoView {
                                     class_name="min-w-36".to_owned()
                                     placeholder=move || t_display!(i18n, rest_client_header_name).to_string()
                                     options={HEADERS_AUTOCOMPLETE}
-                                    on_change=move |_| {}
+                                    on_change=move |_| {
+                                        save_custom_headers(&custom_headers.read_untracked());
+                                    }
                                     value=custom_header.name
                                     set_value=custom_header.set_name
                                 />
@@ -264,7 +272,9 @@ pub fn RestClientPage() -> impl IntoView {
                                     class_name="w-full".to_owned()
                                     placeholder=move || t_display!(i18n, rest_client_header_value).to_string()
                                     options={MEDIA_TYPES_AUTOCOMPLETE}
-                                    on_change=move |_| {}
+                                    on_change=move |_| {
+                                        save_custom_headers(&custom_headers.read_untracked());
+                                    }
                                     value=custom_header.value
                                     set_value=custom_header.set_value
                                 />
@@ -276,6 +286,7 @@ pub fn RestClientPage() -> impl IntoView {
                                     disabled=move || false
                                     on_click=move |_| {
                                         set_custom_headers.write().retain(|h| h.id != custom_header.id);
+                                        save_custom_headers(&custom_headers.read_untracked());
                                     }
                                 />
                             </div>
@@ -324,4 +335,34 @@ fn single_select_option(value: &str) -> (Option<String>, String) {
 
 fn is_base_header_name(name: &str) -> bool {
     name == "content-type" || name == "accept" || name == "accept-language" || name == "user-agent"
+}
+
+fn save_custom_headers(headers: &Vec<CustomHeader>) {
+    let value = headers
+        .iter()
+        .map(|h| format!("{}:{}", h.name.get_untracked(), h.value.get_untracked()))
+        .collect::<Vec<String>>()
+        .join("\n");
+
+    set_local_store_value("rc_custom_headers", value);
+}
+
+fn restore_custom_headers() -> Vec<CustomHeader> {
+    let stored_value = get_local_store_value("rc_custom_headers", "".to_owned());
+    if stored_value.is_empty() {
+        return Vec::new();
+    }
+
+    let mut result = Vec::new();
+    for (i, line) in stored_value.lines().enumerate() {
+        if let Some(index) = line.find(":") {
+            let (name, set_name) = signal(line[..index].to_owned());
+            let (value, set_value) = signal(line[index + 1..].to_owned());
+
+            let header = CustomHeader { id: i + 1, name, set_name, value, set_value };
+            result.push(header);
+        }
+    }
+
+    result
 }
