c428e52063beb11631e8b53e410ae58e609023ca
DimetriusJonson <shtirlizlive@gmail.com>
2026-07-25 15:19:44 +0300

    refactoring

--- app/src/domain/rest_client/rest_client_page.rs
+++ app/src/domain/rest_client/rest_client_page.rs
@@ -24,22 +24,21 @@ pub fn RestClientPage() -> impl IntoView {
     let min_explorer_width = max(256, screen_width / 6);
 
     let (current_request, set_current_request) = signal(RequestInfo::new_empty());
-    let (width, set_width) = signal(
+    let (explorer_width, set_explorer_width) = signal(
         get_local_store_value("rc_explorer_width", min_explorer_width.to_string()).parse::<i32>().unwrap(),
     );
-    let (drag_mode, set_drag_mode) = signal(false);
-
-    let explorer_node_ref = NodeRef::<Div>::new();
-    let dragbar_node_ref = NodeRef::<Div>::new();
+    let (explorer_dragging, set_explorer_dragging) = signal(false);
+    let explorer_ref = NodeRef::<Div>::new();
+    let explorer_dragbar_ref = NodeRef::<Div>::new();
 
     let _ = leptos_dom::helpers::window_event_listener(ev::mousemove, move |ev| {
-        if drag_mode.get() {
-            if let Some(explorer_elem) = explorer_node_ref.get() {
+        if explorer_dragging.get() {
+            if let Some(explorer_elem) = explorer_ref.get() {
                 let rect = explorer_elem.get_bounding_client_rect();
                 let new_width = ev.client_x() - rect.left() as i32;
 
                 if new_width > min_explorer_width && new_width < screen_width / 2 {
-                    set_width.set(new_width);
+                    set_explorer_width.set(new_width);
                     set_local_store_value("rc_explorer_width", new_width.to_string());
                 }
             }
@@ -47,17 +46,17 @@ pub fn RestClientPage() -> impl IntoView {
     });
 
     let _ = leptos_dom::helpers::window_event_listener(ev::mouseup, move |_ev| {
-        set_drag_mode.set(false);
+        set_explorer_dragging.set(false);
     });
 
     view! {
         <div class="flex flex-row dark:text-white">
-            <RestClientExplorer node_ref=explorer_node_ref current_request set_current_request width />
+            <RestClientExplorer node_ref=explorer_ref current_request set_current_request width=explorer_width />
 
-            <div node_ref=dragbar_node_ref class="w-1 bg-gray-700 hover:bg-blue-400/50 cursor-col-resize h-full transition-colors"
+            <div node_ref=explorer_dragbar_ref class="w-1 bg-gray-700 hover:bg-blue-400/50 cursor-col-resize h-full transition-colors"
                 on:mousedown=move |e| {
                     e.prevent_default();
-                    set_drag_mode.set(true);
+                    set_explorer_dragging.set(true);
                 }
             />
 
