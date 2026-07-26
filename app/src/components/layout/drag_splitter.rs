f9fdcead7259e163b04fb5c39c6c1b2ebfa9bcac
DimetriusJonson <shtirlizlive@gmail.com>
2026-07-26 10:55:47 +0300

    compare - dragSplitter

--- app/src/components/layout/drag_splitter.rs
+++ app/src/components/layout/drag_splitter.rs
@@ -1,6 +1,14 @@
-use leptos::{ev, html::Div, leptos_dom, prelude::*};
+use leptos::{
+    ev,
+    html::Div,
+    leptos_dom::{self},
+    prelude::*,
+};
 
-use crate::common::local_store::{get_local_store_value, set_local_store_value};
+use crate::common::{
+    local_store::{get_local_store_value, set_local_store_value},
+    ui_utils::get_browser_width,
+};
 
 #[component]
 pub fn DragSplitter(
@@ -9,6 +17,8 @@ pub fn DragSplitter(
     min_width: i32,
     max_width: i32,
     default_width: i32,
+    #[prop(optional)] class_name: String,
+    #[prop(optional)] allow_mobile: bool,
 ) -> impl IntoView {
     let (dragging, set_dragging) = signal(false);
     let dragbar_ref = NodeRef::<Div>::new();
@@ -17,14 +27,17 @@ pub fn DragSplitter(
         let init_width = get_local_store_value(local_store_prop_name, default_width.to_string())
             .parse::<i32>()
             .unwrap();
-        if let Some(target_elem) = target_ref.get() {
-            (*target_elem).style().set_property("width", &format!("{}px", init_width)).unwrap();
+        if !is_mobile() || allow_mobile {
+            if let Some(target_elem) = target_ref.get() {
+                (*target_elem).style().set_property("width", &format!("{}px", init_width)).unwrap();
+            }
         }
     });
 
     let _ = leptos_dom::helpers::window_event_listener(ev::mousemove, move |ev| {
         if dragging.get_untracked()
             && let Some(target_elem) = target_ref.get()
+            && (!is_mobile() || allow_mobile)
         {
             let rect = target_elem.get_bounding_client_rect();
             let new_width = ev.client_x() - rect.left() as i32;
@@ -42,11 +55,15 @@ pub fn DragSplitter(
     });
 
     view! {
-        <div node_ref=dragbar_ref class="w-1 bg-gray-700 hover:bg-blue-400/50 cursor-col-resize h-full transition-colors"
+        <div node_ref=dragbar_ref class={format!("w-1 bg-gray-700 hover:bg-blue-400/50 cursor-col-resize h-full transition-colors {}", class_name)}
             on:mousedown=move |e| {
                 e.prevent_default();
                 set_dragging.set(true);
             }
         />
     }
+}
+
+fn is_mobile() -> bool {
+    get_browser_width().unwrap() < 768
 }
