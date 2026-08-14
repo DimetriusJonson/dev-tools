14319b7e2452c55b780c3057595b91ae8a5062b3
DimetriusJonson <shtirlizlive@gmail.com>
2026-08-14 14:38:04 +0300

    bug fix

--- app/src/components/ui/code_mirror_editor.rs
+++ app/src/components/ui/code_mirror_editor.rs
@@ -1,4 +1,9 @@
+use crate::{
+    code_mirror::{code_editor_change_lang, init_code_editor, set_code_editor_value},
+    common::ui_utils::safe_updating_ui_value,
+};
 use leptos::prelude::*;
+use web_sys::wasm_bindgen::{JsValue, prelude::Closure};
 
 #[component]
 pub fn CodeMirrorEditor(
@@ -10,71 +15,56 @@ pub fn CodeMirrorEditor(
     #[prop(optional)] hidden: Option<Box<dyn Fn() -> bool + Send + Sync + 'static>>,
     #[prop(optional)] read_only: bool,
 ) -> impl IntoView {
-    #[cfg(not(feature = "ssr"))]
-    {
-        use web_sys::wasm_bindgen::{JsValue, prelude::Closure};
-        use crate::{
-            code_mirror::{code_editor_change_lang, init_code_editor, set_code_editor_value},
-            common::ui_utils::safe_updating_ui_value,
-        };
+    let update_lock = RwSignal::new(false);
+    let (editor_view, set_editor_view) = signal(JsValue::null());
 
-        let update_lock = RwSignal::new(false);
-        let (editor_view, set_editor_view) = signal(JsValue::null());
-        let on_change = Closure::wrap(Box::new(move |new_val: String| {
-            safe_updating_ui_value(update_lock, move || set_value.set(new_val.to_owned()));
-        }) as Box<dyn FnMut(String)>);
+    Effect::new({
+        let element_id = element_id.to_owned();
+        move |_| {
+            let on_change = Closure::wrap(Box::new(move |new_val: String| {
+                safe_updating_ui_value(update_lock, move || set_value.set(new_val.to_owned()));
+            }) as Box<dyn FnMut(String)>);
 
-        Effect::new({
-            let element_id = element_id.to_owned();
-            move |_| {
-                let view = init_code_editor(
-                    &element_id.to_owned(),
-                    &value.get_untracked(),
-                    read_only,
-                    on_change.as_ref(),
-                );
-                set_editor_view.set(view);
-            }
-        });
-
-        Effect::watch(
-            move || value.get(),
-            move |_value, _prev, _| {
-                safe_updating_ui_value(update_lock, move || {
-                    set_code_editor_value(&editor_view.read_untracked(), &value.read_untracked())
-                });
-            },
-            false,
-        );
-
-        let on_change = Closure::wrap(Box::new(move |new_val: String| {
-            safe_updating_ui_value(update_lock, move || set_value.set(new_val.to_owned()));
-        }) as Box<dyn FnMut(String)>);
-
-        Effect::watch(
-            move || lang.get(),
-            move |lang, _prev, _| {
-                code_editor_change_lang(
-                    &editor_view.read_untracked(),
-                    lang,
-                    read_only,
-                    on_change.as_ref(),
-                );
-            },
-            false,
-        );
-    }
-
-    #[cfg(feature = "ssr")]
-    {
-        // prevent compile warning
-        use leptos::leptos_dom::logging::console_log;
-        if read_only {
-            console_log(&lang.read_untracked());
-            *set_value.write_untracked() = "".to_owned();
-            console_log(&value.read_untracked());
+            let view = init_code_editor(
+                &element_id.to_owned(),
+                &value.get_untracked(),
+                read_only,
+                on_change.as_ref(),
+            );
+            set_editor_view.set(view);
+            
+            on_change.forget();
         }
-    }
+    });
+
+    Effect::watch(
+        move || value.get(),
+        move |_value, _prev, _| {
+            safe_updating_ui_value(update_lock, move || {
+                set_code_editor_value(&editor_view.read_untracked(), &value.read_untracked())
+            });
+        },
+        false,
+    );
+
+    Effect::watch(
+        move || lang.get(),
+        move |lang, _prev, _| {
+            let on_change = Closure::wrap(Box::new(move |new_val: String| {
+                safe_updating_ui_value(update_lock, move || set_value.set(new_val.to_owned()));
+            }) as Box<dyn FnMut(String)>);
+
+            code_editor_change_lang(
+                &editor_view.read_untracked(),
+                lang,
+                read_only,
+                on_change.as_ref(),
+            );
+
+            on_change.forget();
+        },
+        false,
+    );
 
     view! {
         <div id={element_id.to_owned()} class={format!("w-full h-0 px-1 md:px-4 py-2 bg-white dark:bg-dark-bg {}", class_name)}
