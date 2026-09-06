e41dfd87ea42a271bba42f83012d6b66a2eea154
DimetriusJonson <shtirlizlive@gmail.com>
2026-09-06 18:51:30 +0300

    loading screen

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -3,5 +3,13 @@ pub fn hydrate() {
     use app::app::App;
     console_error_panic_hook::set_once();
 
+    if let Some(window) = web_sys::window() {
+        if let Some(document) = window.document() {
+            if let Some(body) = document.body() {
+                body.set_inner_html("");
+            }
+        }
+    }
+    
     leptos::mount::mount_to_body(App);
 }
--- server/src/app_router/build_app_router.rs
+++ server/src/app_router/build_app_router.rs
@@ -45,6 +45,15 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                     <script src="/codemirror.min.js"></script>
                 </head>
                 <body class="bg-white dark:bg-dark-bg">
+                    <div class="flex flex-col items-center justify-center min-h-screen w-full dark:text-white">
+                        <div class="flex flex-col items-center space-y-4">
+                            <div class="h-12 w-12 animate-spin rounded-full border-4 border-slate-200 border-t-primary dark:border-slate-700 dark:border-t-primary"></div>
+                            
+                            <p class="text-sm font-medium tracking-wide animate-pulse">
+                                Loading application...
+                            </p>
+                        </div>
+                    </div>
                 </body>
             </html>
         }
