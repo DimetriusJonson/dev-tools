a19f022b2ab1f7d2f1ba03c9d80de3a72e594af6
DimetriusJonson <shtirlizlive@gmail.com>
2026-09-17 22:44:30 +0300

    hide rest client button for mobile screen

--- app/src/components/layout/navbar.rs
+++ app/src/components/layout/navbar.rs
@@ -30,7 +30,7 @@ pub fn Navbar() -> impl IntoView {
                             color=move || nav_button_color(location.pathname.get(), "/compare_text") />
                         <ButtonLink label=move || t_display!(i18n, share_file_btn_label).to_string() href="/share_file".to_owned() button_width=ButtonLinkWidth::Auto
                             color=move || nav_button_color(location.pathname.get(), "/share_file") />
-                        <ButtonLink label=move || t_display!(i18n, rest_client_btn_label).to_string() href="/rest_client".to_owned() button_width=ButtonLinkWidth::Auto
+                        <ButtonLink class_name="hidden md:block".to_owned() label=move || t_display!(i18n, rest_client_btn_label).to_string() href="/rest_client".to_owned() button_width=ButtonLinkWidth::Auto
                             color=move || nav_button_color(location.pathname.get(), "/rest_client") />
                     </div>
 
